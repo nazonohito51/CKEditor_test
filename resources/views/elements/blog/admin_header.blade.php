6dabb3cf742320cdb3fe58956234555b224c86f3
barie <s.kawashima@pepabo.com>
2016-05-08 18:16:34 +0900

    fix navbar

--- resources/views/elements/blog/admin_header.blade.php
+++ resources/views/elements/blog/admin_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-fixed-top">
     <div class="container">
-    <a class="blog-nav-item @if(Request::is('/'))active @endif" href="/entry">HOME</a>
-    <a class="btn btn-danger" href="/api/logout">LOGOUT</a>
-        </div>
+        <a class="blog-nav-item nav navbar-nav navbar-left @if(Request::is('/'))active @endif" href="/entry">HOME</a>
+        <a class="blog-nav-item nav navbar-nav navbar-right btn btn-danger" href="/api/logout">LOGOUT</a>
+    </div>
 </nav>
--- resources/views/elements/blog/header.blade.php
+++ resources/views/elements/blog/header.blade.php
@@ -1,6 +1,6 @@
-<div class="blog-masthead">
+<nav class="navbar navbar-default">
     <div class="container">
-        <a class="blog-nav-item @if(Request::is('/'))active @endif" href="/entry">HOME</a>
-        <a class="btn btn-success" href="/api/login">LOGIN</a>
+        <a class="blog-nav-item nav navbar-nav navbar-left @if(Request::is('/'))active @endif" href="/entry">HOME</a>
+        <a class="blog-nav-item nav navbar-nav navbar-right btn btn-success" href="/api/login">LOGIN</a>
     </div>
-</div>
+</nav>
