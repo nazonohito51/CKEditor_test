adfeab901b388185cb28fc1ffc9efa735a93192e
barie <s.kawashima@pepabo.com>
2016-05-09 20:19:41 +0900

    add public column in entries table

---
+++ database/migrations/2016_05_09_111614_add_public_to_entries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPublicToEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('entries', function (Blueprint $table) {
+            $table->integer('public')->after('body');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('entries', function (Blueprint $table) {
+            $table->dropColumn('public');
+        });
+    }
+}
