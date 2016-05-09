e713ace7c72fa82044bc2f5f0a0fdd87a6c743ba
barie <s.kawashima@pepabo.com>
2016-05-09 20:40:16 +0900

    add display change function

--- public/js/display-switch.js
+++ public/js/display-switch.js
@@ -1,1 +1,27 @@
-$("[name='display-switch']").bootstrapSwitch();
+$("[name='display-switch']").bootstrapSwitch({onText: "公開", offText: "非公開"});
+$("[name='display-switch']").on('switchChange.bootstrapSwitch', function(event, state) {
+    window.alert(state);
+    window.alert($(this).val());
+
+    var entryId = $(this).val();
+    var public = 1;
+    if (state == false) {
+        public = 0;
+    }
+
+    $.ajax({
+        url: '/api/entry/public/' + entryId,
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            'public': public
+        },
+        timeout: 5000,
+        success: function(data) {
+            BootstrapDialog.alert("公開状態の変更に成功しました。");
+        },
+        error: function() {
+            BootstrapDialog.alert("公開状態の変更に失敗しました。");
+        }
+    })
+});
--- resources/views/entry/edit_index.blade.php
+++ resources/views/entry/edit_index.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.blog')
 
-    @section('scripts')
-        <script src="/js/bootstrap-dialog/bootstrap-dialog.min.js"></script>
-        <script src="/js/bootstrap-switch/bootstrap-switch.min.js"></script>
-        <script src="/js/display-switch.js"></script>
-    @stop
+@section('scripts')
+    <script src="/js/bootstrap-dialog/bootstrap-dialog.min.js"></script>
+    <script src="/js/bootstrap-switch/bootstrap-switch.min.js"></script>
+    <script src="/js/display-switch.js"></script>
+    <script>
+        <!--
+
+        -->
+    </script>
+@stop
 
 @section('content')
     <div class="row">
@@ -13,7 +18,7 @@
             @forelse($page as $row)
                 <li class="list-group-item">
                     {{{ $row->title }}}
-                    <input type="checkbox" class="switch-onText switch-offText pull-right" name="display-switch" @if($row->public == 1)checked @endif data-on-text="公開" data-off-text="非公開">
+                    <input type="checkbox" class="pull-right" name="display-switch" value="{{{ $row->id }}}" @if($row->public == 1)checked @endif>
                 </li>
             @empty
                 <p>ブログ記事がありません</p>
