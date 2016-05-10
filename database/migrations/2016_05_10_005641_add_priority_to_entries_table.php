f1d79843998bbbacac735a857c7778ce1be24404
barie <s.kawashima@pepabo.com>
2016-05-10 09:58:16 +0900

    add priority column in entries

---
+++ database/migrations/2016_05_10_005641_add_priority_to_entries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPriorityToEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('entries', function (Blueprint $table) {
+            $table->integer('priority')->after('public');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('entries', function (Blueprint $table) {
+            $table->dropColumn('priority');
+        });
+    }
+}
