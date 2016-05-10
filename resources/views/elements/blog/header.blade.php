d9df4171a70a4f0fe5a4926e41b2c9aa6c24ca0a
barie <s.kawashima@pepabo.com>
2016-05-10 13:48:15 +0900

    fix admin header

--- resources/views/elements/blog/header.blade.php
+++ resources/views/elements/blog/header.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-default">
     <div class="container">
         <a class="blog-nav-item nav navbar-nav navbar-left @if(Request::is('/'))active @endif" href="/entry">HOME</a>
-        <a class="blog-nav-item nav navbar-nav navbar-right btn btn-success" href="/api/login">LOGIN</a>
+        @if($admin !== 1)
+            <a class="blog-nav-item nav navbar-nav navbar-right btn btn-success" href="/api/login">LOGIN</a>
+        @endif
     </div>
 </nav>
