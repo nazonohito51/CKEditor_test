d61684e8295684fefab5a74180ba9b85f9af2260
kawashima <nazonohito51@hotmail.com>
2016-08-12 17:28:30 +0900

    move changeDesign function to sidebar.js

--- public/js/ckeditor/ckeditor.js
+++ public/js/ckeditor/ckeditor.js
@@ -42,26 +42,3 @@ function postEntry(entryId, entryBody) {
         }
     })
 }
-function changeDesign(design_css) {
-    design_url = '/css/' + design_css;
-    design_html = '<!-- @import url(' + design_url + '); -->';
-    $('#blog_design').html(design_html);
-    postDesign(1, design_css)
-}
-function postDesign(userId, design_css) {
-    $.ajax({
-        url: '/api/design/' + userId,
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            'design_css': design_css
-        },
-        timeout: 5000,
-        success: function(data) {
-            BootstrapDialog.alert("保存完了しました。");
-        },
-        error: function() {
-            BootstrapDialog.alert("保存に失敗しました。");
-        }
-    })
-}
--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -10,3 +10,27 @@ $('#sidebar-wrapper').load('/html/sidebar.html', function() {
         }
     });
 });
+
+function changeDesign(design_css) {
+    design_url = '/css/' + design_css;
+    design_html = '<!-- @import url(' + design_url + '); -->';
+    $('#blog_design').html(design_html);
+    postDesign(1, design_css)
+}
+function postDesign(userId, design_css) {
+    $.ajax({
+        url: '/api/design/' + userId,
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            'design_css': design_css
+        },
+        timeout: 5000,
+        success: function(data) {
+            BootstrapDialog.alert("保存完了しました。");
+        },
+        error: function() {
+            BootstrapDialog.alert("保存に失敗しました。");
+        }
+    })
+}
