5dbb1e51be2c40266ebdb93e78ba665bde44d8ad
kawashima <nazonohito51@hotmail.com>
2016-08-16 16:48:46 +0900

    fix admin_header

--- resources/views/elements/blog/admin_header.blade.php
+++ resources/views/elements/blog/admin_header.blade.php
@@ -2,10 +2,14 @@
     <div class="container">
         @if($admin_function == 'index')
             <div class="btn-group">
+                <a type="button" id="sortButton" class="blog-nav-item btn btn-default navbar-btn navbar-left" href="{{ route('entry.create') }}">記事を新規作成する</a>
                 <button type="button" id="sortButton" class="blog-nav-item btn btn-default navbar-btn navbar-left" onclick='sortEntry()'>記事を順番を変更する</button>
             </div>
-        @endif
-        @if($admin_function == 'show')
+        @elseif($admin_function == 'create')
+            <div class="btn-group">
+                <button type="button" id="editButton" class="blog-nav-item btn btn-default navbar-btn navbar-left" onclick='switchEditButton({{{ $entry->id }}})'>記事を編集する</button>
+            </div>
+        @elseif($admin_function == 'show')
             <div class="btn-group">
                 <button type="button" id="editButton" class="blog-nav-item btn btn-default navbar-btn navbar-left" onclick='switchEditButton({{{ $entry->id }}})'>記事を編集する</button>
                 <button type="button" id="sidebarOpener" class="blog-nav-item btn btn-default navbar-btn navbar-left" name="opener">デザインを変更する</button>
